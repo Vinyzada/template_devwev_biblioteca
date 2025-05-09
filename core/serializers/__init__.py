1ea7cf362adcf44ae87ac1a33a5241d714de9a1a
vinyzada <vinyescolas@gmail.com>
2025-05-09 14:41:32 -0300

    feat: inclusão do relacionamento n para n entre Livro e Autor

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,3 +1,5 @@
 from .user import UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
+from .livro import LivroSerializer
+from .autor import AutorSerializer
